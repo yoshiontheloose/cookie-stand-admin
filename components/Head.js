954410f8b25473426848110c3a28cf745cf52ccb
Clarissa P <clarissapamonicutt@gmail.com>
2022-04-15 00:25:50 -0700

    found problem, lab complete

--- components/Head.js
+++
@@ -1,8 +0,0 @@
-export default function Head(){
-  return(
-    <Head>
-      <title>Cookie Stand Admin</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-  )
-}
--- components/header.js
+++
@@ -1,8 +0,0 @@
-export default function Header(){
-  return(
-
-    <header className='p-3 pl-5 bg-emerald-500'>
-      <h1 className='text-3xl'>Cookie Stand Admin</h1>
-    </header>
-  )
-}
